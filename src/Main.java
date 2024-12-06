d846e616ef99b3a2c6f74af613a5c64b114816ee
hamzalis <hamza613468@gmail.com>
2024-12-06 16:56:21 +0300

    Initialize

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Board board = new Board(8); // 8x8'lik bir tahta oluştur
-        board.visit(0, 0); // Knight'ı (0,0) karesine yerleştir
-        System.out.println("Kare (0,0) ziyaret edildi mi? " + board.isVisited(0, 0)); // true
-        board.leave(0, 0); // (0,0) karesini boşalt
-        System.out.println("Kare (0,0) ziyaret edildi mi? " + board.isVisited(0, 0)); // false
-    }
-}
